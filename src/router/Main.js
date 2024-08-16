2de99fe15ffc91040eb01068c49e8ebf4096ea19
AzureAD\박인태 <pit1995@outlook.kr>
2024-08-16 18:30:53 +0900

    오타수정

--- src/router/Main.js
+++ src/router/Main.js
@@ -21,7 +21,7 @@ function Main() {
                     <div>
                         upperside
                     </div>
-                    <SearchBoxox />
+                    <SearchBox />
                     <div>
                         Bottom
                     </div>
