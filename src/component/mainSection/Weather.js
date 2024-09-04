75e8311dd3ac5fa7cdca88b4b44c32da478d2a09
AzureAD\박인태 <pit1995@outlook.kr>
2024-09-04 19:06:28 +0900

    api를 바꾸려고 했으나 해당 api가 더이상 서비스중이지 않았음 그냥 진행 환경변수파일의 위치가 잘못되서 오류가 발생(.env)파일은 루트폴더에 저장해야 적용이 됨

--- src/component/mainSection/Weather.js
+++ src/component/mainSection/Weather.js
@@ -3,45 +3,44 @@ import axios from "axios";
 import weatherDescKo from './util/weatherDescKo';
 
 function Weather() {
-  const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
-  const [ weather, setWeather] = useState(null);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [weather, setWeather] = useState(null);
+
+  console.log("API Key:", API_KEY);
 
   useEffect(() => {
+    const getWeather = async (lat, lon) => {
+      try {
+        const res = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        );
+
+        const cityName = res.data.name;
+        const weatherId = res.data.weather[0].id;
+        const weatherKo = weatherDescKo[weatherId];
+        const weatherIcon = res.data.weather[0].icon;
+        const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+        const temp = Math.round(res.data.main.temp);
+
+        setWeather({
+          description: weatherKo,
+          name: cityName,
+          temp: temp,
+          icon: weatherIconAdrs,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       getWeather(lat, lon);
     });
-  }, []);
-  
-  const getWeather = async(lat, lon) => {
-    try {
-      const res = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-      );
- 
-      const cityName = res.data.name;
-      // id 찾아서 매칭 후 description 한글 번역된 거 가져오기
-      const weatherId = res.data.weather[0].id;
-      const weatherKo = weatherDescKo[weatherId];
-      // 날씨 아이콘 가져오기
-      const weatherIcon = res.data.weather[0].icon;
-      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-      // 소수점 버리기
-      const temp = Math.round(res.data.main.temp);
- 
-      setWeather({
-        description: weatherKo,
-        name: cityName,
-        temp: temp,
-        icon: weatherIconAdrs,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  }, [API_KEY]);
 
-  return(
+  return (
     <div>
       {weather && (
         <div>
