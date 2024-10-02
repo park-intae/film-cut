0a657bb5d5fb21ccfe4c4a8833ab6fc10b83041b
AzureAD\박인태 <pit1995@outlook.kr>
2024-10-03 02:25:10 +0900

    searchBox 파트 css 조정 중

--- src/router/Main.js
+++ src/router/Main.js
@@ -3,10 +3,15 @@ import styles from './Main.module.css';
 import { SearchBox, Today } from './../component/mainSection';
 import { Memo, TodoList } from './../component/bottom';
 import { GoogleAuthLogin } from './../component/upperSide';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faPen, faBars } from '@fortawesome/free-solid-svg-icons';
 
 
 function Main() {
     const [loading, setLoading] = useState(true);
+    const flexCenter = { justifyContent: 'center'};
+    const flexEnd = { justifyContent: 'flex-end'};
+    const flexAlign = { alignItems: 'center'};
 
     useEffect(() => {
         setTimeout(() => {
@@ -22,17 +27,22 @@ function Main() {
             ) : (
                 <div>
                     <div>
-                        <GoogleAuthLogin />
+                        <div><GoogleAuthLogin /></div>
                     </div>
-                    <hr/>
-                    <SearchBox />
+                    <hr />
+                    <div className={styles.divFlex} style={{ ...flexCenter, ...flexAlign }}>
+                        <SearchBox />
+                        <button className={styles.circle}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                    </div>
                     <Today />
-                    <button className={styles.circle}>'</button>
-                    <button></button>
-                    <hr/>
+                    <div className={styles.divFlex} style={flexEnd}>
+                        <button className={styles.circle}><FontAwesomeIcon icon={faPen} /></button>
+                        <button className={styles.circle}><FontAwesomeIcon icon={faBars} /></button>
+                    </div>
+                    <hr />
                     <div>
                         <Memo />
-                        <hr/>
+                        <hr />
                         <TodoList />
                     </div>
                 </div>
